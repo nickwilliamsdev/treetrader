730ad54e7cff88913811e21c68fcb6b81d29832a
nickwilliamsdev <nickwilliams112358@proton.me>
2025-07-23 09:57:42 -0700

    display to understand data gen

---
+++ display_gen_data.py
@@ -0,0 +1,12 @@
+from utils.synthetic_data_service import SyntheticWienerGenerator, SyntheticOHLCVGenerator
+from matplotlib import pyplot as plt
+ohlc_gen_service = SyntheticOHLCVGenerator(n_steps=1000, mu=-0.00, sigma=0.01, dt=1, seed=42)
+
+test_df = ohlc_gen_service.generate(start=100.0)
+
+plt.plot(test_df['time'], test_df['close'], label='Synthetic Close Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.title('Synthetic OHLCV Data')
+plt.legend()
+plt.show()
