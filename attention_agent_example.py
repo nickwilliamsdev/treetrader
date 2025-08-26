a460eb96dd884970ffbbf53e6eb174637571e53e
nickwilliamsdev <nickwilliams112358@proton.me>
2025-08-26 03:43:41 -0700

    setting up diff evo

--- attention_agent_example.py
+++ attention_agent_example.py
@@ -5,7 +5,7 @@ from utils.synthetic_data_service import SyntheticOHLCVGenerator
 import pandas as pd
 import matplotlib.pyplot as plt
 from diffevo import DDIMScheduler, BayesianGenerator
-
+from torch.nn.utils import parameters_to_vector
 
 if __name__ == '__main__':
     dummy_df = SyntheticOHLCVGenerator(n_steps=500, mu=0.001, sigma=0.1, dt=1, seed=42).generate(start=100.0)
@@ -15,12 +15,16 @@ if __name__ == '__main__':
     # Define hyperparameters for the model
     STATE_DIM = len(dummy_df.columns)  # e.g., Open, High, Low, Close, Volume, and other indicators
     ACTION_DIM = 3  # e.g., Buy, Sell, Hold
-    SEQ_LEN = 60    # Look-back window of 60 timesteps
+    SEQ_LEN = 400   # Look-back window of 400 timesteps
+    POP_SIZE = 100  # Population size for the evolutionary algorithm
+
+    scheduler = DDIMScheduler(num_step=SEQ_LEN)
 
     # Instantiate the model
     agent_model = CausalAttentionAgent(state_dim=STATE_DIM, action_dim=ACTION_DIM, seq_len=SEQ_LEN)
-    
-    
+    dim = parameters_to_vector(agent_model.parameters()).shape[0]
+    x = torch.randn(POP_SIZE, dim)
+
     # The `window_size` here should match the `seq_len` of your PyTorch model
     env = TradingEnv(df=dummy_df, window_size=SEQ_LEN)
 
@@ -42,8 +46,6 @@ if __name__ == '__main__':
         observation, reward, done, truncated, info = env.step(action)
         
         rewards.append(reward)  # Store the reward
-        
-        print(f"Action: {action}, Reward: {reward:.2f}, Done: {done}, Truncated: {truncated}")
         
         if done or truncated:
             print("Episode finished.")
