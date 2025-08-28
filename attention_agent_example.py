d5c8e43b0dd1a5c17aa8c30c6dd5903ff7c9f9ab
nickwilliamsdev <nickwilliams112358@proton.me>
2025-08-27 20:33:12 -0700

    making example trading optimization with diffusion evolution

--- attention_agent_example.py
+++ attention_agent_example.py
@@ -7,6 +7,23 @@ import matplotlib.pyplot as plt
 from diffevo import DDIMScheduler, BayesianGenerator
 from torch.nn.utils import parameters_to_vector
 
+def run(agent):
+    rewards = []
+    # Example of a random walk in the environment
+    for _ in range(100):
+        # Convert observation to a PyTorch tensor and add batch dimension
+        batched_observation = torch.unsqueeze(torch.tensor(observation, dtype=torch.float32), dim=0)
+
+        # Get action from the model
+        action = torch.argmax(agent(batched_observation), dim=-1)
+        observation, reward, done, truncated, info = env.step(action)
+        
+        rewards.append(reward)  # Store the reward
+        
+        if done or truncated:
+            return sum(rewards)
+    return sum(rewards)
+
 if __name__ == '__main__':
     dummy_df = SyntheticOHLCVGenerator(n_steps=500, mu=0.001, sigma=0.1, dt=1, seed=42).generate(start=100.0)
     dummy_df = dummy_df[['time', 'open', 'high', 'low', 'close', 'volume']].copy()
@@ -17,6 +34,7 @@ if __name__ == '__main__':
     ACTION_DIM = 3  # e.g., Buy, Sell, Hold
     SEQ_LEN = 400   # Look-back window of 400 timesteps
     POP_SIZE = 100  # Population size for the evolutionary algorithm
+    SCALING = .1 # scaling for fitness
 
     scheduler = DDIMScheduler(num_step=SEQ_LEN)
 
@@ -24,32 +42,18 @@ if __name__ == '__main__':
     agent_model = CausalAttentionAgent(state_dim=STATE_DIM, action_dim=ACTION_DIM, seq_len=SEQ_LEN)
     dim = parameters_to_vector(agent_model.parameters()).shape[0]
     x = torch.randn(POP_SIZE, dim)
+    
+    # list for training rewards and history
+    reward_history = []
+    population_history = [x * SCALING]
+    x0_population = [x * SCALING]
+    observations = []
 
     # The `window_size` here should match the `seq_len` of your PyTorch model
     env = TradingEnv(df=dummy_df, window_size=SEQ_LEN)
 
-    # Reset the environment
-    observation, info = env.reset()
-    
-    print("Initial observation shape:", observation.shape)
-    
-    # Store rewards for visualization
-    rewards = []
 
-    # Example of a random walk in the environment
-    for _ in range(100):
-        # Convert observation to a PyTorch tensor and add batch dimension
-        batched_observation = torch.unsqueeze(torch.tensor(observation, dtype=torch.float32), dim=0)
 
-        # Get action from the model
-        action = torch.argmax(agent_model(batched_observation), dim=-1)
-        observation, reward, done, truncated, info = env.step(action)
-        
-        rewards.append(reward)  # Store the reward
-        
-        if done or truncated:
-            print("Episode finished.")
-            break
     plt.plot(env.history)
     plt.xlabel("Time Step")
     plt.ylabel("Portfolio Value")
