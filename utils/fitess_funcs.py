49be8660da0470a0707a84a075c2e8f9792aa158
nickwilliamsdev <nickwilliams112358@proton.me>
2025-09-02 15:18:05 -0700

    diffusion evolution working

--- utils/fitess_funcs.py
+++ utils/fitess_funcs.py
@@ -13,17 +13,18 @@ def batched_fitness_function(agent, states, price_changes):
         torch.Tensor: A tensor of shape (batch, 1) containing the overall return for each asset.
     """
     batch, ep_len, seq_len, features = states.shape
-    rewards = torch.zeros(batch, 1)  # Initialize rewards for each asset
+    rewards = torch.zeros(batch, 1, device=states.device)  # Initialize rewards for each asset
 
     for t in range(ep_len - 1):  # Iterate over episode timesteps
         # Get the current state for all assets at time t
         current_states = states[:, t, :, :]  # Shape: (batch, seq_len, features)
+
         # Get actions for all assets in the batch
         actions = agent(current_states)  # Shape: (batch, 2) -> 2 actions: Buy, Sell
         actions = torch.argmax(actions, dim=-1)  # Convert to discrete actions (0=Sell, 1=Buy)
 
         # Convert actions to -1 (Sell) and 1 (Buy)
-        actions = actions.float() * 2 - 1  # Map (0, 1) -> (-1, 1)
+        #actions = actions.float() * 2 - 1  # Map (0, 1) -> (-1, 1)
 
         # Calculate rewards as the product of actions and percentage changes
         rewards += (actions * price_changes[:, t + 1]).unsqueeze(-1)  # Shape: (batch, 1)
