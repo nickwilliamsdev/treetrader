0c28048f75691e8cdc49abcf6ebf4ba95ea05846
nickwilliamsdev <nickwilliamsdevstuff112358@proton.me>
2026-02-22 06:10:21 -0800

    implement tradingbase problem for topk ranknet

--- utils/neat_trading_problem.py
+++ utils/neat_trading_problem.py
@@ -2,16 +2,9 @@ import jax, jax.numpy as jnp
 from tensorneat.problem.base import BaseProblem
 
 class TradingProblemBase(BaseProblem):
-  jitable = True
-
-  def __init__(
-      self,
-      epoch_len,
-      pop_size,
-  ):
-    super().__init__()
-    self.epoch_len = epoch_len
-    self.pop_size = pop_size
+  
+  def __init__(self):
+    super.__init__()
 
   @property
   def input_data_len(self):
